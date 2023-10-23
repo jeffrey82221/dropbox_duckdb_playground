daf0061dcea76a079d5297f528193e965d3de463
jeffrey82221 <jeffrey82221@gmail.com>
2023-10-23 22:34:55 +0800

    feature: add vaex testing

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -2,9 +2,10 @@ import pytest
 from src.operator import Operator
 from typing import List 
 import pandas as pd
+import vaex as vx
 from src.filesystem import LocalBackend
-from src.storage import PandasStorage
-class MyOperator(Operator):
+from src.storage import PandasStorage, VaexStorage
+class PDOperator(Operator):
     @property
     def input_ids(self):
         return [
@@ -20,26 +21,68 @@ class MyOperator(Operator):
     def transform(self, inputs: List[pd.DataFrame]) -> List[pd.DataFrame]:
         return [inputs[0] + inputs[1]]
 
-
+class VXOperator(Operator):
+    @property
+    def input_ids(self):
+        return [
+            'input1',
+            'input2'
+        ]
+    
+    @property
+    def output_ids(self):
+        return [
+            'output'
+        ]
+    
+    def transform(self, inputs: List[vx.DataFrame]) -> List[vx.DataFrame]:
+        return [vx.concat([inputs[0], inputs[1]])]
+    
 @pytest.fixture
-def operator():
+def pd_op():
     storage = PandasStorage(LocalBackend())
     storage.upload(pd.DataFrame([1,2,3]), 'input1')
     storage.upload(pd.DataFrame([1,1,1]), 'input2')
-    op = MyOperator(
+    op = PDOperator(
         input_storage=storage,
         output_storage=storage
         )
     return op
 
+@pytest.fixture
+def vx_op():
+    storage = VaexStorage(LocalBackend('./tmp/'))
+    in_table = pd.DataFrame(
+        [[1,2,3]], columns=['a', 'b', 'c']
+    )
+    in_table = vx.from_pandas(in_table)
+    storage.upload(in_table, 'input1')
+    in_table = pd.DataFrame(
+        [[1,1,1]], columns=['a', 'b', 'c']
+    )
+    in_table = vx.from_pandas(in_table)
+    storage.upload(in_table, 'input2')
+    op = VXOperator(
+        input_storage=storage,
+        output_storage=storage
+        )
+    return op
 
-def test_get_input_type(operator):
-    assert operator.get_input_type() == pd.DataFrame
+def test_get_input_type(pd_op, vx_op):
+    assert pd_op.get_input_type() == pd.DataFrame
+    assert vx_op.get_input_type() == vx.DataFrame
 
-def test_get_output_type(operator):
-    assert operator.get_output_type() == pd.DataFrame
+def test_get_output_type(pd_op, vx_op):
+    assert pd_op.get_output_type() == pd.DataFrame
+    assert vx_op.get_output_type() == vx.DataFrame
 
-def test_execute(operator):
-    operator.execute()
-    output = operator._output_storage.download('output')
+def test_execute(pd_op, vx_op):
+    pd_op.execute()
+    output = pd_op._output_storage.download('output')
     pd.testing.assert_frame_equal(output, pd.DataFrame([2,3,4]))
+    vx_op.execute()
+    output = vx_op._output_storage.download('output')
+    pd.testing.assert_frame_equal(
+        output.to_pandas_df(), 
+        pd.DataFrame([[1,2,3],[1,1,1]], columns=['a', 'b', 'c'])
+    )
