bc1397d14117985add2f04408bfe97d25c5ae18c
Jeffrey Lin <jeffrey82221@gmail.com>
2023-10-25 08:53:55 +0800

    Update rdb.py

--- src/rdb.py
+++ src/rdb.py
@@ -1,3 +1,8 @@
+"""
+RDB classes: 
+Can register table, execute sql, and extract table object.
+"""
+import abc
 import duckdb
 from .backend import Backend
 
@@ -11,6 +16,28 @@ class RDB(Backend):
         self._conn = conn
         super().__init__()
 
+    @abc.abstractmethod
+    def register(self, table_name: str, table: object):
+        """
+        Register a table object into RDB by a table_name
+        Args:
+            - table_name: The table name.
+            - table: The table object.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def execute(self, sql: str) -> object:
+        """
+        Execute a sql and extract selected table as a table object.
+
+        Args:
+            - sql: The sql to be executed.
+        Returns:
+            - object: The table object.
+        """
+        raise NotImplementedError
+        
 
 class DuckDBBackend(RDB):
     def __init__(self):
@@ -19,5 +46,5 @@ class DuckDBBackend(RDB):
     def register(self, table_name: str, table: object):
         self._conn.register(table_name, table)
 
-    def execute(self, sql):
+    def execute(self, sql: str) -> object:
         return self._conn.execute(sql)
