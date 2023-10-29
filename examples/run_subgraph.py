db6a88df3b1e525cc65c767f122c43527311346f
jeffrey82221 <jeffrey82221@gmail.com>
2023-10-29 16:13:05 +0800

    refactor: modulize subgraph layer

---
+++ examples/run_subgraph.py
@@ -0,0 +1,16 @@
+from batch_framework.rdb import DuckDBBackend
+from batch_framework.filesystem import LocalBackend
+from batch_framework.storage import PandasStorage
+from subgraph.links import LinkExtractor
+from subgraph.nodes import NodeExtractor
+from subgraph.validate import Validation
+
+if __name__ == '__main__':
+    input_fs = LocalBackend('./data/canon/output/')
+    db = DuckDBBackend()
+    link_op = LinkExtractor(db, input_fs=input_fs)
+    node_op = NodeExtractor(db, input_fs=input_fs)
+    val_op = Validation(PandasStorage(db))
+    link_op.execute()
+    node_op.execute()
+    val_op.execute()
