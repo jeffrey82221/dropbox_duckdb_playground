154351255e1e7a33427a9f85b3eeef189913b213
jeffrey82221 <jeffrey82221@gmail.com>
2023-11-25 21:13:17 +0800

    refactor: allow node / link extraction sqls to be defined from metagraph class

--- examples/run_subgraph.py
+++ examples/run_subgraph.py
@@ -29,6 +29,161 @@ metagraph = MetaGraph({
         'latest_requirement',
         'latest_url'
     ],
+    node_sqls={
+        # Main Package Node
+        'package': """
+        SELECT
+            DISTINCT ON (pkg_name)
+            HASH(pkg_name) AS node_id,
+            name,
+            package_url,
+            project_url,
+            requires_python,
+            version,
+            keywords,
+            num_releases
+        FROM latest_package
+        """,
+        # Requirement Package Node
+        'requirement': """
+        SELECT
+            DISTINCT ON (requirement)
+            HASH(requirement) AS node_id,
+            requirement AS name
+        FROM latest_requirement
+        """,
+        # Author Person Node
+        'author': """
+        SELECT
+            DISTINCT ON (author, author_email)
+            HASH(CONCAT(author, '|', author_email)) AS node_id,
+            author AS name,
+            author_email AS email
+        FROM latest_package
+        WHERE author IS NOT NULL AND author_email IS NOT NULL
+        AND author_email <> ''
+        """,
+        # Maintainer Person Node
+        'maintainer': """
+        SELECT
+            DISTINCT ON (maintainer, maintainer_email)
+            HASH(CONCAT(maintainer, '|', maintainer_email)) AS node_id,
+            maintainer AS name,
+            maintainer_email AS email
+        FROM latest_package
+        WHERE maintainer IS NOT NULL AND maintainer_email IS NOT NULL
+        AND maintainer_email <> ''
+        """,
+        # License Node
+        'license': """
+        SELECT
+            DISTINCT ON (license)
+            HASH(license) AS node_id,
+            license AS name
+        FROM latest_package
+        WHERE license IS NOT NULL 
+            AND license <> 'UNKNOWN'
+            AND license <> 'LICENSE.txt'
+        """,
+        # Docs URL Node
+        'docs_url': """
+        SELECT
+            DISTINCT ON (docs_url)
+            HASH(docs_url) AS node_id,
+            docs_url AS url
+        FROM latest_package
+        WHERE docs_url IS NOT NULL
+        """,
+        # Home Page URL Node
+        'home_page': """
+        SELECT
+            DISTINCT ON (home_page)
+            HASH(home_page) AS node_id,
+            home_page AS url
+        FROM latest_package
+        WHERE home_page IS NOT NULL
+        """,
+        # Project URL Node
+        'project_url': """
+        SELECT
+            DISTINCT ON (url)
+            HASH(url) AS node_id,
+            url
+        FROM latest_url
+        WHERE url IS NOT NULL
+        AND url <> 'UNKNOWN'
+        """
+    },
+    link_sqls={
+        # Has Requirement Link
+        'has_requirement': """
+        SELECT
+            DISTINCT ON (pkg_name, requirement)
+            HASH(pkg_name) AS from_id,
+            HASH(requirement) AS to_id
+        FROM latest_requirement
+        """,
+        # Has Author Link
+        'has_author': """
+        SELECT
+            DISTINCT ON (author, author_email, pkg_name)
+            HASH(pkg_name) AS from_id,
+            HASH(CONCAT(author, '|', author_email)) AS to_id
+        FROM latest_package
+        WHERE author IS NOT NULL AND author_email IS NOT NULL
+        AND author_email <> ''
+        """,
+        # Has Maintainer Link
+        'has_maintainer': """
+        SELECT
+            DISTINCT ON (maintainer, maintainer_email, pkg_name)
+            HASH(pkg_name) AS from_id,
+            HASH(CONCAT(maintainer, '|', maintainer_email)) AS to_id
+        FROM latest_package
+        WHERE maintainer IS NOT NULL AND maintainer_email IS NOT NULL
+        AND maintainer_email <> ''
+        """,
+        # Has License Link
+        'has_license': """
+        SELECT
+            DISTINCT ON (license, pkg_name)
+            HASH(pkg_name) AS from_id,
+            HASH(license) AS to_id
+        FROM latest_package
+        WHERE license IS NOT NULL 
+            AND license <> 'UNKNOWN'
+            AND license <> 'LICENSE.txt'
+        """,
+        # Docs URL Node
+        'has_docs_url': """
+        SELECT
+            DISTINCT ON (docs_url, pkg_name)
+            HASH(pkg_name) AS from_id,
+            HASH(docs_url) AS to_id
+        FROM latest_package
+        WHERE docs_url IS NOT NULL
+        """,
+        # Home Page URL Node
+        'has_home_page': """
+        SELECT
+            DISTINCT ON (home_page, pkg_name)
+            HASH(pkg_name) AS from_id,
+            HASH(home_page) AS to_id
+        FROM latest_package
+        WHERE home_page IS NOT NULL
+        """,
+        # Project URL Node
+        'has_project_url': """
+        SELECT
+            DISTINCT ON (url, pkg_name)
+            HASH(pkg_name) AS from_id,
+            HASH(url) AS to_id,
+            url_type
+        FROM latest_url
+        WHERE url IS NOT NULL
+        AND url <> 'UNKNOWN'
+        """
+    }
 )
 
 input_fs = LocalBackend('./data/canon/output/')
--- examples/subgraph/metagraph.py
+++ examples/subgraph/metagraph.py
@@ -1,9 +1,24 @@
 from typing import Dict, Tuple, List
 
 class MetaGraph:
-    def __init__(self, metagraph: Dict[str, Tuple[str, str]], input_ids: List[str]):
+    """
+    Data Class holding extraction logic of subgraphs
+
+    Args:
+        - metagraph: describe the relationship between subgraph nodes and links
+            - key: link name
+            - value (tuple)
+                - value[0]: name of the source node for the link
+                - value[1]: name of the target node for the link
+        - input_ids: name of tables from which the subgraph is extracted.
+        - node_sqls: define how node tables are extracted from the tables of `input_ids`
+        - link_sqls: define how link tables are extracted from the tables of `input_ids`
+    """
+    def __init__(self, metagraph: Dict[str, Tuple[str, str]], input_ids: List[str], node_sqls: Dict[str, str], link_sqls: Dict[str, str]):
         self._metagraph = metagraph
         self.input_ids = input_ids
+        self.node_sqls = node_sqls
+        self.link_sqls = link_sqls
 
     @property
     def nodes(self) -> List[str]:
